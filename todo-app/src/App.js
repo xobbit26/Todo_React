7e379ab714b513ee20bb5c1a0c7514be44c5adec
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2020-02-21 17:12:07 +0300

    added node modules to ignore and simple hooks for testing

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  // По принципу componentDidMount и componentDidUpdate:
+  useEffect(() => {
+    // Обновляем заголовок документа, используя API браузера
+    console.log('init');
+  });
+
   return (
-    <div className="App">
+    <div>
+      <p>Вы нажали {count} раз</p>
+      <button onClick={() => setCount(count + 1)}>
+        Нажми на меня
+      </button>
     </div>
   );
 }
