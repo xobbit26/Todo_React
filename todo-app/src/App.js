1dcb9911cd516d8012590871daa2b7f71b57d8d0
xobbit26@gmail.com <xobbit26@gmail.com>
2020-02-24 23:59:38 +0300

    start doing completedTodoList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './index.css';
 import TodoList from './TodoList';
 import { TodoContext } from './context';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
   const [todoTitle, setTodoTitle] = useState('');
+  const [completedTodos, setCompletedTodos] = useState([]);
 
   useEffect(() => {
     const raw = localStorage.getItem('todos')
@@ -20,14 +21,12 @@ export default function App() {
 
   const onAddTodo = (event) => {
     if (event.key === 'Enter' && todoTitle !== '') {
-      setTodos([
-        ...todos,
-        {
-          id: Date.now(),
-          title: todoTitle,
-          isCompleted: false
-        }]
-      );
+      const newTodo = {
+        id: Date.now(),
+        title: todoTitle,
+        isCompleted: false
+      };
+      setTodos([...todos, newTodo]);
       setTodoTitle('');
     }
   }
@@ -38,24 +37,43 @@ export default function App() {
     }));
   }
 
-  const onCheckTodoItem = (todoId) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === todoId) {
-        todo.isCompleted = !todo.isCompleted
-      }
-      return todo;
-    }));
+  const onCheckTodoItem = (event) => {
+    const { todoId, isChecked } = event.target;
+
+    if (isChecked) {
+      const completedTodo = todos.find((todo) => todo.id === todoId);
+      setCompletedTodos([...completedTodos, completedTodo]);
+      onRemoveItem(completedTodo.id);
+
+    } else {
+      const todo = completedTodos.find((todo) => todo.id === todoId);
+      setTodos([...todos, todo]);
+      setCompletedTodos(completedTodos.filter((todo) => {
+        return todo.id !== todoId;
+      }));
+    }
+
   }
 
   return (
-    <div>
+    <div className="containeer">
+      <h1>Todo app</h1>
       <TodoContext.Provider value={{ onRemoveItem, onCheckTodoItem }}>
-        <input type="text"
-          value={todoTitle}
-          onChange={(event) => setTodoTitle(event.target.value)}
-          onKeyPress={onAddTodo}
-        />
+        <div className="input-field">
+          <input type="text"
+            value={todoTitle}
+            onChange={(event) => setTodoTitle(event.target.value)}
+            onKeyPress={onAddTodo}
+            placeholder={"Todo name"}
+          />
+        </div>
         <TodoList items={todos} />
+
+        {todos.length !== 0 &&
+          <hr />
+        }
+        <TodoList items={completedTodos} />
+
       </TodoContext.Provider>
     </div>
   );
--- todo-app/src/TodoItem.js
+++ todo-app/src/TodoItem.js
@@ -4,14 +4,18 @@ import { TodoContext } from './context';
 export default function TodoItem({ id, title, isCompleted }) {
     const { onRemoveItem, onCheckTodoItem } = useContext(TodoContext);
 
+    const cls = isCompleted? "todo completed" : "todo";
+
     return (
         <span>
-            <li>
-                <input type="checkBox"
-                    checked={isCompleted}
-                    onChange={() => onCheckTodoItem(id)} />
-                {title}
-                <span onClick={() => onRemoveItem(id)}>X</span>
+            <li className={cls}>
+                <label>
+                    <input type="checkBox"
+                        checked={isCompleted}
+                        onChange={(event) => onCheckTodoItem(event)} />
+                    <span>{title}</span>
+                    <span onClick={() => onRemoveItem(id)}>X</span>
+                </label>
             </li>
         </span>
     )
