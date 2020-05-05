421fd67f3c6d45a16dbcbfbee6544a80537bb074
xobbit26@gmail.com <xobbit26@gmail.com>
2020-05-05 23:39:46 +0300

    added react bootstrap

--- app/src/components/body/Body.jsx
+++ app/src/components/body/Body.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 import './body.css';
 
 export default function Body() {
     return (
-        <div className="app-body">this will be body</div>
+        <div className="app-body">
+            <InputGroup className="mb-3">
+                <FormControl placeholder="Input your new todo"/>
+                <InputGroup.Append>
+                    <Button variant="outline-secondary">Button</Button>
+                </InputGroup.Append>
+            </InputGroup>
+        </div>
     )
 }
