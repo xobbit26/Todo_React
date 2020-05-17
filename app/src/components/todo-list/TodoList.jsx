1c2952cb207b8f3d743283a11d8254d3620e0458
xobbit26@gmail.com <xobbit26@gmail.com>
2020-05-17 16:56:54 +0300

    adjusted todo-item component

--- app/src/components/todo-list/TodoList.jsx
+++ app/src/components/todo-list/TodoList.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import './todo-list.css';
+
 function TodoItem({ todoItem }) {
     console.log("todoItem", todoItem)
     return (
-        <div>
-            <div>{todoItem.text}</div>
-            <div>{todoItem.createdOn}</div>
+        <div className="row">
+            <div className="col-md-2">
+                <input type="checkbox" checked={todoItem.completed} />
+            </div>
+            <div className="col-md-6 todo-item-text">
+                {todoItem.text}
+            </div>
+            <div className="col-md-2">{todoItem.createdOn}</div>
+            <div className="col-md-2"><a type="link" title="delete">x</a></div>
         </div>
     )
 }
